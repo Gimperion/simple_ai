5f1f74140e64d3950edfbcdc1bcfacaf8ae4dd06
Tommy Shen <gimperion@gmail.com>
2024-10-18 22:52:50 -0400

    clean up syntax

--- lib/converse.ex
+++ lib/converse.ex
@@ -1,4 +1,8 @@
 defmodule Converse do
+  @moduledoc """
+  This module is responsible for handling the conversation with the Anthropix API.
+  """
+
   defp get_client do
     case System.get_env("ANTHROPIC_API_KEY") do
       nil ->
@@ -8,7 +12,18 @@ defmodule Converse do
     end
   end
 
+  @doc """
+  Passes payoad to API.
+
+  ## Examples
+    payload = [
+      model: "claude-3-haiku-20240307",
+      messages: [%{role: "user", content: "Say hello back!"}]
+    ]
+    response = Converse.run_convo(payload)
+  """
   def run_convo(payload) do
-    Anthropix.chat(get_client(), payload)
+    get_client()
+    |> Anthropix.chat(payload)
   end
 end
---
+++ lib/simple_ai.ex
@@ -0,0 +1,18 @@
+defmodule SimpleAI do
+  @moduledoc """
+  Documentation for `SimpleAI`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SimpleAI.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
